fb0c179180360f32aaeed03ceee72e5d4ab007d8
Jesse Ilc <jesseilc123@gmail.com>
2023-06-01 18:03:30 -0500

    Code along complete

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,29 @@
 class MySet:
+    def __init__(self, enumerable  = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+    
+    def clear(self):
+        self.dictionary.clear()
+
+    def __str__(self):
+        newSet= []
+        for key in self.dictionary.keys():
+            newSet.append(str(key))
+        return f'MySet: {{{",".join(newSet)}}}'
+            
